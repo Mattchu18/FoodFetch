2ca0fff0cd9da4a3ecf1a59c9259b74ffcbe4f4b
Matthew <mattchu18@gmail.com>
2023-06-13 10:54:23 -0700

    added to_dict to review model

--- app/models/review.py
+++ app/models/review.py
@@ -12,7 +12,17 @@ class Review(db.Model):
     restaurant_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("restaurants.id")))
     rating = db.Column(db.Integer(), nullable = False)
     review_text = db.Column(db.String(1000), nullable = False)
-
+    created_at = db.Column(db.DateTime(), default = datetime.now)
     #relationships
     user = db.relationship("User", back_populates = "review")
     restaurant = db.relationship("Restaurant", back_populates = "review")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'restaurant_id': self.restaurant_id,
+            'rating': self.rating,
+            'review_text': self.review_text,
+            'created_at': self.created_at
+        }
