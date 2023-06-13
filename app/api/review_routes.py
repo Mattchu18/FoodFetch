e22051b996cdab890f6435ca6b4eaff955736902
Matthew <mattchu18@gmail.com>
2023-06-13 13:35:43 -0700

    added get all review route

--- app/api/review_routes.py
+++ app/api/review_routes.py
@@ -4,4 +4,13 @@ from flask_login import login_required, current_user
 from app.models import User
 from app.models.db import db
 from app.forms.review_form import ReviewForm
-review_routes = Blueprint('reviews', __name__, url_prefix='')
+reviews = Blueprint('reviews', __name__, url_prefix='')
+
+@reviews.route('/')
+def get_all_reviews():
+    '''
+    Gets all reviews
+    '''
+    all_reviews_obj = Review.query.all()
+    all_reviews = [review.to_dict() for review in all_reviews_obj]
+    return all_reviews
