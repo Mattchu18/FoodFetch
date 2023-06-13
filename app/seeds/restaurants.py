312d193c0960240c9b019566ac0c59f49324c190
Matthew <mattchu18@gmail.com>
2023-06-13 13:56:33 -0700

    seeded restaurants

---
+++ app/seeds/restaurants.py
@@ -0,0 +1,59 @@
+from app.models.restaurant import db, Restaurant, environment, SCHEMA
+from sqlalchemy.sql import text
+from datetime import datetime, time
+
+def seed_restaurant():
+    restaurant_data = [
+        {
+        'user_id': 1,
+        'name': "McDonald's",
+        'address': "1 McDonald Way",
+        'phone_number': "1200000001",
+        'cuisine_type': "American",
+        'opening_time': time(hour=8, minute=0),
+        'closing_time': time(hour=22, minute=0)
+        },
+        {
+        'user_id': 1,
+        'name': "Jollibee",
+        'address': "2 Jollibee Way",
+        'phone_number': "1200000002",
+        'cuisine_type': "Filipino",
+        'opening_time': time(hour=8, minute=0),
+        'closing_time': time(hour=22, minute=0)
+        },
+        {
+        'user_id': 1,
+        'name': "Panda Express",
+        'address': "3 Panda Way",
+        'phone_number': "1200000003",
+        'cuisine_type': "Chinese",
+        'opening_time': time(hour=8, minute=0),
+        'closing_time': time(hour=22, minute=0)
+        },
+    ]
+
+    for data in restaurant_data:
+        restaurant = Restaurant(
+            user_id = data['user_id'],
+            name = data['name'],
+            address = data['address'],
+            phone_number = data['phone_number'],
+            cuisine_type = data['cuisine_type'],
+            opening_time = data['opening_time'],
+            closing_time = data['closing_time']
+        )
+
+        db.session.add(restaurant)
+
+    db.session.commit()
+    print("Restaurants successfully seeded!")
+
+
+def under_restaurant():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.restaurants RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM restaurants"))
+
+    db.session.commit()
