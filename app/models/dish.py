003a69203626897a85f274d6aef5dfbba60ab934
Matthew <mattchu18@gmail.com>
2023-06-13 11:20:29 -0700

    added attributes to dish model. need relationships

---
+++ app/models/dish.py
@@ -0,0 +1,16 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+class Dish(db.Model):
+    __tablename__ = "dishes"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key = True)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("restaurants.id")), nullable = False)
+    name = db.Column(db.String(200), nullable = False)
+    description = db.Column(db.String(1000), nullable = False)
+    price = db.Column(db.Integer, nullable = False)
+
+    #relationships
