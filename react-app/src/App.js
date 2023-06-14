8ecfc18b02a9f38603571b87cb2a074f717d8899
Matthew <mattchu18@gmail.com>
2023-06-14 09:23:58 -0700

    edited app.js for typo, and added reviews to db

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -21,13 +21,13 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
-          <Route path="/login" >
+          <Route exact path="/login" >
             <LoginFormPage />
           </Route>
-          <Route path="/signup">
+          <Route exact path="/signup">
             <SignupFormPage />
           </Route>
-          <Route exact patch="/reviews" component={GetUserReviews}/>
+          <Route exact path="/reviews" component={GetUserReviews}/>
         </Switch>
       )}
     </>
