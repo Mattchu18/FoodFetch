e4efac504986645951f1f28e46b8e9062f63ba88
Matthew <mattchu18@gmail.com>
2023-06-15 16:27:11 -0700

    added all restaurant dishes thunk in store

---
+++ react-app/src/store/dish.js
@@ -0,0 +1,72 @@
+// const GET_ONE_DISH = "dish/oneDish"
+const GET_ALL_DISHES = "dish/allDishes"
+const GET_ALL_RESTAURANT_DISHES = "dish/allRestaurantDishes"
+const CREATE_DISH = "dish/createDish"
+const DELETE_DISH = "dish/deleteDish"
+const EDIT_DISH = "dish/editDish"
+
+
+// const getOneDish = (dish) => ({
+//     type: GET_ONE_DISH,
+//     dish
+// })
+
+const getAllDishes = (dishes) => ({
+    type: GET_ALL_DISHES,
+    dishes
+})
+
+const getAllRestaurantDishes = (dishes) => ({
+    type: GET_ALL_RESTAURANT_DISHES,
+    dishes
+})
+
+const createDish = (dish) => ({
+    type: CREATE_DISH,
+    dish
+})
+
+const deleteDish = (dish) => ({
+    type: DELETE_DISH,
+    dish
+})
+
+const editDish = (dish) => ({
+    type: EDIT_DISH,
+    dish
+})
+
+
+// export const thunkOneDish = (dishId) => {
+
+// }
+
+
+export const thunkAllRestaurantDishes = (restaurant) => async (dispatch) => {
+    const response = await fetch(`/api/restuarants/${restaurant.id}`)
+    if (response.ok) {
+        const data = await Response.json()
+        dispatch(getAllRestaurantDishes(data))
+    }
+}
+
+
+const initialState = { allRestaurantDishes: {} }
+const dishReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_ALL_RESTAURANT_DISHES: {
+            const newState = {}
+            const allRestaurantDishes = action.dishes
+            allRestaurantDishes.forEach(dish => {
+                newState[dish.id] = dish
+            })
+            return {
+                ...state,
+                allRestaurantDishes: newState
+            }
+        }
+        default: return state
+    }
+}
+
+export default dishReducer
