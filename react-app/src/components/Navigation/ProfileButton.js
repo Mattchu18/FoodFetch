7ca461bfff0ca61bf587feafe157190ee6980977
Matthew <mattchu18@gmail.com>
2023-06-15 15:30:29 -0700

    added styling to navigation to make sticky

--- react-app/src/components/Navigation/ProfileButton.js
+++ react-app/src/components/Navigation/ProfileButton.js
@@ -4,9 +4,10 @@ import { logout } from "../../store/session";
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from "../LoginFormModal";
 import SignupFormModal from "../SignupFormModal";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 function ProfileButton({ user }) {
+  const history = useHistory()
   const dispatch = useDispatch();
   const [showMenu, setShowMenu] = useState(false);
   const ulRef = useRef();
@@ -33,7 +34,9 @@ function ProfileButton({ user }) {
 
   const handleLogout = (e) => {
     e.preventDefault();
-    dispatch(logout());
+    dispatch(logout())
+    .then(closeMenu)
+    history.push("/")
   };
 
   const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
