d0a6b155ca23a30e2e35b71cc1dc9c45004aed6f
Matthew <mattchu18@gmail.com>
2023-06-15 14:46:41 -0700

    added styling to home page

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 import { thunkAllRestaurants } from "../../store/restaurant"
 import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
-
+import "./index.css"
 
 const Home = () => {
     const dispatch = useDispatch()
@@ -15,85 +15,100 @@ const Home = () => {
 
 
     return (
-        <div>
-            <div id="cuisine_types">
-                <div>
-                    <i class="fa-solid fa-burger"></i>
+        <div id="home-center">
+            <div id="home-container">
+                <div id="cuisine-container">
+                    <div id="cuisine-types">
+                        <div className="icon">
+                            <i class="fa-solid fa-burger"></i>
+                        </div>
+                        <div className="cuisine-text">
+                            <p>American</p>
+                        </div>
+                    </div>
+                    <div id="cuisine-types">
+                        <div className="icon">
+                            <i class="fa-solid fa-burger"></i>
+                        </div>
+                        <div className="cuisine-text">
+                            <p>Filipino</p>
+                        </div>
+                    </div>
+                    <div id="cuisine-types">
+                        <div className="icon">
+                            <i class="fa-solid fa-burger"></i>
+                        </div>
+                        <div className="cuisine-text">
+                            <p>Chinese</p>
+                        </div>
+                    </div>
+                    <div id="cuisine-types">
+                        <div className="icon">
+
+                            <i class="fa-solid fa-burger"></i>
+                        </div>
+                        <div className="cuisine-text">
+                            <p>Italian</p>
+                        </div>
+                    </div>
+                    <div id="cuisine-types">
+                        <div className="icon">
+                            <i class="fa-solid fa-burger"></i>
+                        </div>
+                        <div className="cuisine-text">
+                            <p>Korean</p>
+                        </div>
+                    </div>
+                    <div id="cuisine-types">
+                        <div className="icon">
+                            <i class="fa-solid fa-burger"></i>
+                        </div>
+                        <div className="cuisine-text">
+                            <p>Mediterranean</p>
+                        </div>
+                    </div>
+                    <div id="cuisine-types">
+                        <div className="icon">
+                            <i class="fa-solid fa-burger"></i>
+                        </div>
+                        <div className="cuisine-text">
+                            <p>Vietnamese</p>
+                        </div>
+                    </div>
+                    <div id="cuisine-types">
+                        <div className="icon">
+                            <i class="fa-solid fa-burger"></i>
+                        </div>
+                        <div className="cuisine-text">
+                            <p>Peruvian</p>
+                        </div>
+                    </div>
+                    <div id="cuisine-types">
+                        <div className="icon">
+                            <i class="fa-solid fa-burger"></i>
+                        </div>
+                        <div className="cuisine-text">
+                            <p>Nepalese</p>
+                        </div>
+                    </div>
+                    <div id="cuisine-types">
+                        <div className="icon">
+                            <i class="fa-solid fa-burger"></i>
+                        </div>
+                        <div className="cuisine-text">
+                            <p>Indian</p>
+                        </div>
+                    </div>
                 </div>
-                <div>
-                    <p>American</p>
-                </div>
-            </div>
-            <div id="cuisine_types">
-                <div>
-                    <i class="fa-solid fa-burger"></i>
-                </div>
-                <div>
-                    <p>Filipino</p>
-                </div>
-            </div>
-            <div id="cuisine_types">
-                <div>
-                    <i class="fa-solid fa-burger"></i>
-                </div>
-                <div>
-                    <p>Chinese</p>
-                </div>
-            </div>
-            <div id="cuisine_types">
-                <div>
-                    <i class="fa-solid fa-burger"></i>
-                </div>
-                <div>
-                    <p>Italian</p>
-                </div>
-            </div>
-            <div id="cuisine_types">
-                <div>
-                    <i class="fa-solid fa-burger"></i>
-                </div>
-                <div>
-                    <p>Korean</p>
-                </div>
-            </div>
-            <div id="cuisine_types">
-                <div>
-                    <i class="fa-solid fa-burger"></i>
-                </div>
-                <div>
-                    <p>Mediterranean</p>
-                </div>
-            </div>
-            <div id="cuisine_types">
-                <div>
-                    <i class="fa-solid fa-burger"></i>
-                </div>
-                <div>
-                    <p>Vietnamese</p>
-                </div>
-            </div>
-            <div id="cuisine_types">
-                <div>
-                    <i class="fa-solid fa-burger"></i>
-                </div>
-                <div>
-                    <p>Peruvian</p>
-                </div>
-            </div>
-            <div id="cuisine_types">
-                <div>
-                    <i class="fa-solid fa-burger"></i>
-                </div>
-                <div>
-                    <p>Nepalese</p>
-                </div>
-            </div>
-            <div id="cuisine_types">
-                <div>
-                    <i class="fa-solid fa-burger"></i>
-                </div>
-                <div>
-                    <p>Indian</p>
+
+                <div id="promo-container">
+                    <div className="promo-header">
+                        <h2>Under $5 dishes</h2>
+                        <span> arrows</span>
+                    </div>
+                    <div className="promo-featured-container">
+                        <span>Dishes coming soon</span>
+                    </div>
                 </div>
             </div>
         </div>
