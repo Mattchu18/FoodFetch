7b02d7b0584bd9f179fcef2c81ea3b3367ccda66
Matthew <mattchu18@gmail.com>
2023-06-15 17:49:39 -0700

    added css for modal to be dynamic

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from "react-redux"
 import { Link } from "react-router-dom"
 import "./index.css"
 
+const cuisineTypeArr = ["American", "Filipino", "Chinese", "Italian", "Korean", "Mediterranean", "Vietnamese", "Peruvian", "Nepalese", "Indian"]
+
 const Home = () => {
     const dispatch = useDispatch()
     const allRestaurantsObj = useSelector(state => state.restaurant.allRestaurants)
@@ -18,89 +20,18 @@ const Home = () => {
         <div id="home-center">
             <div id="home-container">
                 <div id="cuisine-container">
-                    <div id="cuisine-types">
-                        <div className="icon">
-                            <i class="fa-solid fa-burger"></i>
+                    {cuisineTypeArr.map(cuisine => (
+                        <div id="cuisine-types">
+                            <div className="icon">
+                                <i class="fa-solid fa-burger"></i>
+                            </div>
+                            <div className="cuisine-text">
+                                <span>{cuisine}</span>
+                            </div>
                         </div>
-                        <div className="cuisine-text">
-                            <p>American</p>
-                        </div>
-                    </div>
-                    <div id="cuisine-types">
-                        <div className="icon">
-                            <i class="fa-solid fa-burger"></i>
-                        </div>
-                        <div className="cuisine-text">
-                            <p>Filipino</p>
-                        </div>
-                    </div>
-                    <div id="cuisine-types">
-                        <div className="icon">
-                            <i class="fa-solid fa-burger"></i>
-                        </div>
-                        <div className="cuisine-text">
-                            <p>Chinese</p>
-                        </div>
-                    </div>
-                    <div id="cuisine-types">
-                        <div className="icon">
+                    ))}
 
-                            <i class="fa-solid fa-burger"></i>
-                        </div>
-                        <div className="cuisine-text">
-                            <p>Italian</p>
-                        </div>
-                    </div>
-                    <div id="cuisine-types">
-                        <div className="icon">
-                            <i class="fa-solid fa-burger"></i>
-                        </div>
-                        <div className="cuisine-text">
-                            <p>Korean</p>
-                        </div>
-                    </div>
-                    <div id="cuisine-types">
-                        <div className="icon">
-                            <i class="fa-solid fa-burger"></i>
-                        </div>
-                        <div className="cuisine-text">
-                            <p>Mediterranean</p>
-                        </div>
-                    </div>
-                    <div id="cuisine-types">
-                        <div className="icon">
-                            <i class="fa-solid fa-burger"></i>
-                        </div>
-                        <div className="cuisine-text">
-                            <p>Vietnamese</p>
-                        </div>
-                    </div>
-                    <div id="cuisine-types">
-                        <div className="icon">
-                            <i class="fa-solid fa-burger"></i>
-                        </div>
-                        <div className="cuisine-text">
-                            <p>Peruvian</p>
-                        </div>
-                    </div>
-                    <div id="cuisine-types">
-                        <div className="icon">
-                            <i class="fa-solid fa-burger"></i>
-                        </div>
-                        <div className="cuisine-text">
-                            <p>Nepalese</p>
-                        </div>
-                    </div>
-                    <div id="cuisine-types">
-                        <div className="icon">
-                            <i class="fa-solid fa-burger"></i>
-                        </div>
-                        <div className="cuisine-text">
-                            <p>Indian</p>
-                        </div>
-                    </div>
                 </div>
-
                 <div id="promo-container">
                     <div className="promo-header">
                         <h2>Under $5 dishes</h2>
