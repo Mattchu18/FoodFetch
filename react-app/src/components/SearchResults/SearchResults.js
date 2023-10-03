1d0ad9556e9e24a14681acec215d802ba39b5c0e
Matthew <mattchu18@gmail.com>
2023-10-03 14:04:54 -0700

    started styling search results component

--- react-app/src/components/SearchResults/SearchResults.js
+++ react-app/src/components/SearchResults/SearchResults.js
@@ -15,13 +15,15 @@ const SearchResults = () => {
 
     return (
         <div>
-            <div>
+            <div className="search-results-container">
                 {!restaurantResults?.length && (<h3>No results found</h3>)}
                 {restaurantResults.map(restaurant => (
                     <Link to={`/restaurants/${restaurant.id}`}>
-                        <div>
+                        <div className="search-results-mapped">
                             <div>
+                                <img src={restaurant.image}/>
                                 <h3>{restaurant.name}</h3>
+                                <span>{restaurant.cuisine_type}</span>
                             </div>
                         </div>
                     </Link>
