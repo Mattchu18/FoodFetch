862f1a252b936c7769a1923903a1fe43dae74a3e
Matthew <mattchu18@gmail.com>
2023-10-05 09:33:47 -0700

    displaying more search results details

--- react-app/src/components/SearchResults/SearchResults.js
+++ react-app/src/components/SearchResults/SearchResults.js
@@ -2,6 +2,7 @@ import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { thunkAllRestaurants } from "../../store/restaurant"
 import { Link } from "react-router-dom"
+import "./SearchResults.css"
 
 const SearchResults = () => {
     const dispatch = useDispatch()
@@ -20,10 +21,14 @@ const SearchResults = () => {
                 {restaurantResults.map(restaurant => (
                     <Link to={`/restaurants/${restaurant.id}`}>
                         <div className="search-results-mapped">
-                            <div>
-                                <img src={restaurant.image}/>
+                            <img src={restaurant.image} />
+                            <div className="search-results-details">
                                 <h3>{restaurant.name}</h3>
                                 <span>{restaurant.cuisine_type}</span>
+                                <span>{restaurant.address}</span>
+                                <span>Opens at {restaurant.opening_time}</span>
+                                <span>Closes at {restaurant.closing_time}</span>
+
                             </div>
                         </div>
                     </Link>
